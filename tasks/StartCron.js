10da8f851e1f204fbe495b60241c3e2dd1733ae4
Benjamin E <ben@dk8.co>
2019-08-04 16:35:40 +0100

    log cron dates

--- tasks/StartCron.js
+++ tasks/StartCron.js
@@ -1,14 +1,21 @@
 const cron = require('node-cron')
 
+const logDate = () => {
+	console.log(new Date())
+}
+
 const StartCron = (func = () => {}) => {
 	// Every Wednesday and Saturday at 21:00
 	const cronPattern = '* 21 * * Wednesday,Saturday'
 	cron.schedule(cronPattern, () => {
+		console.log('Run tasks')
+		logDate()
 		func()
 	}, {
 		scheduled: true,
 		timezone: 'Europe/London',
 	})
+	console.log('Start cron')
 }
 
 module.exports = StartCron
