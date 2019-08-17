b05eb4ba41848bdc65267bb8e37d1f7f0f0c1a60
Benjamin E <ben@dk8.co>
2019-08-17 11:50:03 +0100

    edit tweet text

--- tasks/BuildTweet.js
+++ tasks/BuildTweet.js
@@ -29,21 +29,21 @@ function formTweet(data) {
 	}
 
 	tweetText1 += `On ${prettyDate}, `
-
+	tweetText1 += `${cashWon} and ${freeTicketsWon} free tickets were won `
+	tweetText1 += `with ${ticketsBought + previousFreeTickets}. `
 	tweetText1 += `${moneySpent} bought ${ticketsBought} tickets`
 	if (previousFreeTickets) {
-		tweetText1 += ` with an additional ${previousFreeTickets} free tickets used from the previous draw`
+		tweetText1 += ` with an additional ${previousFreeTickets} free tickets used from the previous draw. `
+	} else {
+		tweetText1 += '. '
 	}
 
-	tweetText1 += '.\n\n'
-
-	tweetText1 += `${cashWon} and ${freeTicketsWon} free tickets were won, `
-	tweetText1 += `resulting in a ${earningsWord} of ${roundEarnings}.\n`
+	tweetText1 += `This results in a ${earningsWord} of ${roundEarnings} for this round.\n`
 
 	// SECOND TWEET
-	tweetText2 += `After ${roundsPlayed} ${roundsWord}, ${accumulatedSpend} has been spent and `
-	tweetText2 += `${accumulatedEarnings} has been won, `
-	tweetText2 += `resulting in an overall ${accumulatedEarningsWord} of ${accumulatedProfitLoss}.`
+	tweetText2 += `${accumulatedEarnings} has now been won `
+	tweetText2 += `after ${roundsPlayed} ${roundsWord}, and a total spend of ${accumulatedSpend}. `
+	tweetText2 += `The overall ${accumulatedEarningsWord} is currently ${accumulatedProfitLoss}.`
 
 	return [tweetText1, tweetText2]
 }
